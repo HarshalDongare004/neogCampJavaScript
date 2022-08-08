664e57cf54eb196705a05f6129b50eb00822cd0d
HarshalDongare004 <harshaldongare004@gmail.com>
2022-08-08 10:38:21 +0530

    ex 2 complete

--- js/app.js
+++ js/app.js
@@ -1,2 +1,17 @@
-let check = prompt("Your name please ? ")
-alert("It is running " + check)
+// let check = prompt("Your name please ? ")
+// alert("It is running " + check)
+
+// function clickBtn() {
+//     alert("btn was clicked")
+// }
+  
+let btnTranslate = document.querySelector('#btn-translate')
+//     .addEventListener("click", function clickBtn() {
+//     alert("btn was clicked")
+// }
+//   )
+
+btnTranslate.addEventListener("click", function clickEventHandler() {
+    console.log("Btn was clicked")
+  })
+// console.log(btnTranslate)
